8399f9d67af6be488b48c96c9770602fc1539fee
Plague <36628963+OFWModz@users.noreply.github.com>
2021-03-23 08:23:49 +0000

    Added Test WinForm And Moved To Newtonsoft!

---
+++ JsonConfigTest/JsonConfigTest/JsonConfigTest.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace ConfigTest
+{
+    partial class JsonConfigTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(374, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(180, 18);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Readable?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(437, 131);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.ForeColor = System.Drawing.Color.Magenta;
+            this.textBox3.Location = new System.Drawing.Point(142, 20);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(279, 97);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.BackColor = System.Drawing.Color.Black;
+            this.numericUpDown2.DecimalPlaces = 3;
+            this.numericUpDown2.ForeColor = System.Drawing.Color.Magenta;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown2.Location = new System.Drawing.Point(7, 97);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            32767,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 74);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(53, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Test3";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.ForeColor = System.Drawing.Color.Magenta;
+            this.textBox2.Location = new System.Drawing.Point(7, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.Color.Black;
+            this.numericUpDown1.ForeColor = System.Drawing.Color.Magenta;
+            this.numericUpDown1.Location = new System.Drawing.Point(7, 20);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            32767,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // JsonConfigTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(461, 183);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Magenta;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "JsonConfigTest";
+            this.ShowIcon = false;
+            this.Text = "JsonConfigTest";
+            this.Load += new System.EventHandler(this.ConfigTest_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ JsonConfigTest/JsonConfigTest/JsonConfigTest.cs
@@ -0,0 +1,115 @@
+﻿using Libraries;
+using System;
+using System.Windows.Forms;
+
+namespace ConfigTest
+{
+    public partial class JsonConfigTest : Form
+    {
+        public static JsonConfigTest Instance;
+
+        public Configuration configuration = new Configuration();
+
+        /// <summary>
+        /// An Example Of How To Automate The Entire Config Process, Even For WinForms.
+        /// </summary>
+        public class Configuration
+        {
+            public bool Readable
+            {
+                get => Instance.checkBox1.Checked;
+                set => Instance.checkBox1.Checked = value;
+            }
+
+            public int Test1
+            {
+                get => (int)Instance.numericUpDown1.Value;
+                set => Instance.numericUpDown1.Value = value;
+            }
+
+            public string Test2
+            {
+                get => Instance.textBox2.Text;
+                set => Instance.textBox2.Text = value;
+            }
+
+            public bool Test3
+            {
+                get => Instance.checkBox2.Checked;
+                set => Instance.checkBox2.Checked = value;
+            }
+
+            public float Test4
+            {
+                get => (float)Instance.numericUpDown2.Value;
+                set => Instance.numericUpDown2.Value = (decimal)value;
+            }
+
+            public string[] Test5
+            {
+                get => Instance.textBox3.Text.Replace("\r", "").Split('\n');
+                set => Instance.textBox3.Text = string.Join("\r\n", value);
+            }
+        }
+
+        public JsonConfigTest()
+        {
+            InitializeComponent();
+        }
+
+        private string ConfigPath = Environment.CurrentDirectory + "\\TestConfig.json";
+
+        private void ConfigTest_Load(object sender, EventArgs e)
+        {
+            Instance = this;
+
+            var Result = JsonConfig.LoadConfig(ref configuration, ConfigPath);
+
+            MessageBox.Show(Result.Item1 + " " + Result.Item2, "Info");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var Result = JsonConfig.LoadConfig(ref configuration, ConfigPath);
+
+            MessageBox.Show(Result.Item1 + " " + Result.Item2, "Info");
+        }
+
+        public void Save()
+        {
+            var Result = JsonConfig.SaveConfig(configuration, ConfigPath, checkBox1.Checked);
+
+            MessageBox.Show(Result.Item1 + " " + Result.Item2, "Info");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //Save();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //Save();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            //Save();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            //Save();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            //Save();
+        }
+    }
+}
